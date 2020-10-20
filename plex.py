346e0118113dd6d390c0e8243e4d269a10716bec
Christian Dreier <flipper.x3r@googlemail.com>
2020-10-20 03:14:35 +0200

    Refactoring and additional messages

--- plex.py
+++ plex.py
@@ -7,41 +7,46 @@ import plexapi.myplex as myplex
 
 SERVER = "Some server"
 LIBSECTION = "Some section"
-SERIES = "Some series"
+SHOW = "Some series"
 
 
 starterdir = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 
-class PlexAccount:
-    def __init__(self):
-        with open(os.path.join(starterdir, "secrets.json"), encoding="utf8") as f:
-            data = json.load(f)
-        cred = data["plexAccount"]
-
-        self.user = cred["user"]
-        self.password = cred["password"]
+def log(*args, sep=" ", end="\n"):
+    print(*args, sep=sep, end=end)
+    sys.stdout.flush()
 
 
 def extract_filename(episode):
     return episode.locations[0].split("/")[-1]
 
 
-account = PlexAccount()
-instance = myplex.MyPlexAccount(account.user, account.password)
-server = instance.resource(SERVER).connect()
-show = server.library.section(LIBSECTION).get(SERIES)
+with open(os.path.join(starterdir, "secrets.json"), encoding="utf8") as f:
+    data = json.load(f)
+credentials = data["plexAccount"]
+
+episodes = (myplex.MyPlexAccount(credentials["user"], credentials["password"])
+    .resource(SERVER)
+    .connect()
+    .library
+    .section(LIBSECTION)
+    .get(SHOW)
+    .episodes())
+
+log(f'Downloading {len(episodes)} episodes of "{SHOW}"...')
 
 outdir = os.path.join(starterdir, "out")
 if not os.path.exists(outdir):
     os.mkdir(outdir)
 
-for episode in show.episodes():
+for episode in episodes:
     filename = extract_filename(episode)
     if os.path.exists(os.path.join(outdir, filename)):
-        print(f'"{filename}" exists already; skipping')
+        log(f'"{filename}" exists already; skipping')
     else:
-        print(f'Download "{filename}"')
+        log(f'Download "{filename}"...', end="\t")
         episode.download(outdir, keep_original_name=True)
+        log("Ready")
 
-print("All episodes downloaded")
+log("All episodes downloaded")
